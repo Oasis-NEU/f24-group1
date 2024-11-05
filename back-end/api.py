ce875a62d4fcb251b193c78b9383ca96341b5283
patriots1 <charugundla.s@northeastern.edu>
2024-11-05 00:26:08 -0500

    current changes

--- back-end/api.py
+++ back-end/api.py
@@ -15,10 +15,5 @@ def search():
     return jsonify({"message": "content recieved", "content": content}), 200
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8001))
-    app.run(debug=True, host='0.0.0.0', port=port)
-
-
 
 
