1ee96f3ce88152b379cb65cc574ff915cfa9c1ca
patriots1 <charugundla.s@northeastern.edu>
2024-11-05 01:26:34 -0500

    final commit

--- back-end/api.py
+++ back-end/api.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 def search():
     # convert bytes to string
     content = request.data.decode('utf-8')
-    #return 400 if no content provided
+    # return 400 if no content provided
     retrieve_top_three_object = RetrieveTopThree()
     if not content:
         return jsonify({"error": "no input provided"}), 400
-    
+    # create an object of RetrieveTopThree, and call the prove_work_return_json method.
     l = retrieve_top_three_object.prove_work_return_json(content)
     
     return jsonify(l), 200
