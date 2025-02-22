c5866a65c39388c81da01101419943b8240dff6f
nourshoreibah <nour17372@icloud.com>
2025-02-22 12:21:21 -0500

    cors fix

--- back-end/api.py
+++ back-end/api.py
@@ -10,7 +10,7 @@ import serverless_wsgi
 REACT_BUILD_FOLDER = os.path.abspath("../front-end/react-app/dist")
 
 app = Flask(__name__, static_folder=REACT_BUILD_FOLDER, template_folder=REACT_BUILD_FOLDER)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, origins=["http://54.86.145.39:5000"])
 load_dotenv()
 
 
